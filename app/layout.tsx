ff93c997a8d9812ecf0eec67388a8a8b5ebbdd1c
delmac <delalitengue.dev@gmail.com>
2024-11-26 05:21:45 +0000

    made changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,22 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <header className="w-full h-14 border-b border-neutral-300 flex">
+          <div className="px-10 h-full flex items-center justify-center">
+            <a href="/" className="text-xl font-bold uppercase">LOGO</a>
+          </div>
+          <div className="flex-1 h-full flex items-center justify-start gap-14">
+            <a href="/groups/a" className="text-lg font-medium">A</a>
+            <a href="/groups/b" className="text-lg font-medium">B</a>
+            <a href="/groups/c" className="text-lg font-medium">C</a>
+          </div>
+          <div className="px-10 h-full flex items-center justify-center">
+            <a href="/stats" className="text-lg font-medium">Stats</a>
+          </div>
+        </header>
+        <main className="w-full flex justify-center py-8">
+          {children}
+        </main>
       </body>
     </html>
   );
