d66f781b87a1d6cc375255c45d300ff54fc245f8
delmac <delalitengue.dev@gmail.com>
2024-11-28 17:35:59 +0000

    made edits

--- components/common/home.tsx
+++ components/common/home.tsx
@@ -2,7 +2,8 @@ import { useGetCollections } from "@/lib/query-hooks";
 import { TabsContent } from "@radix-ui/react-tabs";
 
 export default function Home() {
-    // const { data: collections, isLoading, isError } = useGetCollections();
+    const { data: collections, isLoading, isError } = useGetCollections();
+    console.log({collections});
 
     return (
         <TabsContent value="home" className="tab-content">
